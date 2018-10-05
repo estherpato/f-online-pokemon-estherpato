33380a8da9e833c403c1027b3393e0ba9cf83488
estherpato <estherpato@gmail.com>
2018-10-05 19:08:47 +0200

    Finish styling

--- src/components/FilterInput.js
+++ src/components/FilterInput.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import '../stylesheets/FilterInput.css';
 
 class FilterInput extends Component {
     render() {
         return (
-            <form>
-                <label></label>
-                <input/>
+            <form className="input-form">
+                <label className="input-label">Filtra Pokemons por nombre...</label>
+                <input
+                    className="input"
+                    type="text"
+                    placeholder="Filtra Pokemons por nombre"
+                    value="" />
             </form>
         );
     }
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import PokeCard from './PokeCard.js';
+import '../stylesheets/PokeList.css';
 
 class PokeList extends Component {
     render() {
         const { pokeArray } = this.props;
         return (
-            <div>
-
-                <ul>
-                    {pokeArray
-                    .sort((a,b) => a.id - b.id)
+            <ul className="poke-list">
+                {pokeArray
+                    .sort((a, b) => a.id - b.id)
                     .map((pokemon) => {
                         return (
                             <li key={pokemon.id}>
-                                <PokeCard 
+                                <PokeCard
                                     name={pokemon.name}
                                     id={pokemon.id}
                                     image={pokemon.sprite.front_default}
@@ -22,8 +21,7 @@ class PokeList extends Component {
                             </li>
                         );
                     })}
-                </ul>
-            </div>
+            </ul>
         );
     }
 }
