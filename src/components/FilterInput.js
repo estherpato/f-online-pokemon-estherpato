80bd3f9a33b160beb559431f9feb27aca4d9fb0c
Renato PL <renapl90@gmail.com>
2018-10-09 10:02:41 +0300

    form elements like inputs should be linked to a label for a11y

--- src/components/FilterInput.js
+++ src/components/FilterInput.js
@@ -5,10 +5,12 @@ class FilterInput extends Component {
     render() {
         return (
             <div className="input-form">
-                <label className="input-label">Filtra Pokemons por nombre...</label>
+                <label for="searchbar" className="input-label">Filtra Pokemons por nombre...</label>
                 <input
                     className="input"
                     type="text"
+                    id="searchbar"
+                    name="searchbar"
                     placeholder="Filtra Pokemons por nombre..."
                     onChange={this.props.findMatches}
                 />
